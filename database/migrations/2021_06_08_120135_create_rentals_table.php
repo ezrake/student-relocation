77b1b5032a8b29630fb396ade9614df69a5c6575
Ezra Akeba <akebaezra@gmail.com>
2021-06-08 15:44:40 +0300

    Create rentals and rental units tables

---
+++ database/migrations/2021_06_08_120135_create_rentals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('location_id');
+            $table->json('pics_uri');
+            $table->string('name');
+            $table->string('type');
+            $table->json('description');
+            $table->timestamps();
+
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentals');
+    }
+}
