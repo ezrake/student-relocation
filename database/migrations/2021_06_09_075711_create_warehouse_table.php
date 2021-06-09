d9d982cd8229142f08ee69e9bc09168cd91434ca
Ezra Akeba <akebaezra@gmail.com>
2021-06-09 16:50:02 +0300

    Create warehouse table

---
+++ database/migrations/2021_06_09_075711_create_warehouse_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWarehouseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warehouse', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('admin_id');
+            $table->unsignedBigInteger('location_id');
+            $table->string('address');
+            $table->timestamps();
+
+            $table->foreign('admin_id')->references('id')->on('users');
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warehouse');
+    }
+}
