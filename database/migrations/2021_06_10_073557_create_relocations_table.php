20b997086244c3e457cc0e80f4c8c306e08adf97
Ezra Akeba <akebaezra@gmail.com>
2021-06-11 09:55:29 +0300

    Create relocations table

---
+++ database/migrations/2021_06_10_073557_create_relocations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRelocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relocations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->json('from');
+            $table->json('to');
+            $table->enum('status', ['pending', 'overdue', 'complete']);
+            $table->boolean('to_warehouse');
+            $table->date('due_date');
+            $table->date('completed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relocations');
+    }
+}
