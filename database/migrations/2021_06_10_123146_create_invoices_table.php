7faaf30c29ed97a46fe704c7d58b0f6d5640c620
Ezra Akeba <akebaezra@gmail.com>
2021-06-11 09:59:01 +0300

    Create invoice and invoice_details tables

---
+++ database/migrations/2021_06_10_123146_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('service_id');
+            $table->date('due_date');
+            $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('users');
+            $table->foreign('service_id')->references('id')->on('services');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
