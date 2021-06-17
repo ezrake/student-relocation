e34669d5a6f31410df8adfdbcb228f2cb1a5284d
Ezra Akeba <akebaezra@gmail.com>
2021-06-17 11:37:59 +0300

    Add relationships to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        $this->belongsTo(Role::class);
+    }
+
+    public function student()
+    {
+        $this->hasMany(Student::class);
+    }
 }
