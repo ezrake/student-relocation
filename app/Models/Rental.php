0599fccd6307449229f1e92c8c3a7a3f70ae75ea
Ezra Akeba <akebaezra@gmail.com>
2021-07-02 12:36:25 +0300

    Add relations and fillables to Rental model

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Rental extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'location_id',
+        'pics_uri',
+        'name',
+        'description'
+    ];
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
 }
